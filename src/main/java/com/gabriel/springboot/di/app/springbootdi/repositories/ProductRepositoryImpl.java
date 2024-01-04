e7a99a5917cafe54200ae9aa30aa72c242738e69
Gabriel Martinez <132589481+BiarqGabriel@users.noreply.github.com>
2024-01-04 13:53:16 -0300

    first commit

---
+++ src/main/java/com/gabriel/springboot/di/app/springbootdi/repositories/ProductRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.gabriel.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.gabriel.springboot.di.app.springbootdi.models.Product;
+
+@Component
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepositoryImpl() {
+        this.data = Arrays.asList(
+                new Product(1L, "Computer", 1000L),
+                new Product(2L, "Mouse", 100L),
+                new Product(3L, "Keyboard", 150L),
+                new Product(4L, "Monitor", 500L)
+        );
+    }
+    
+    @Override
+    public List<Product> findAll() {
+        return data;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return data.stream().filter(p -> p.getId().equals(id)).findFirst().orElse(null);
+    }
+
+
+}
---
+++ src/main/java/com/gabriel/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -0,0 +1,37 @@
+package com.gabriel.springboot.di.app.springbootdi.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.gabriel.springboot.di.app.springbootdi.models.Product;
+import com.gabriel.springboot.di.app.springbootdi.repositories.ProductRepository;
+
+
+@Component
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll().stream().map(
+                p -> {
+                    Double priceImp = p.getPrice() * 1.25d;
+                    // Product newProduct = new Product(p.getId(), p.getName(),
+                    // priceImp.longValue());
+                    Product newProduct = (Product) p.clone();
+                    newProduct.setPrice(priceImp.longValue());
+                    return newProduct;
+                }).collect(Collectors.toList());
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+}
